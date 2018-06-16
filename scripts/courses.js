47017997c001de34097676da42fadf11c5d5c976
brownben <9870007+brownben@users.noreply.github.com>
2018-06-16 21:01:13 +0100

    Feature: Add In-Program Course Editing
    Added the ability to create, modify and deleete individual courses in the program

--- scripts/courses.js
+++ scripts/courses.js
@@ -27,6 +27,76 @@ const hideCoursesInfo = function () {
     document.getElementById('courses-info').setAttribute('style', 'display:none')
 }
 
+// Basic Course functions add, update, delete
+function addCourse () {
+    coursesInfo('blank')
+    if (document.getElementById('courses-add-name').value === '') {
+        coursesInfo('error', ' Error: No Name specified for Course ')
+    }
+    else {
+        try {
+            coursesDB.insert({
+                name: document.getElementById('courses-add-name').value,
+                climb: document.getElementById('courses-add-climb').value,
+                length: Math.round(parseFloat(document.getElementById('courses-add-length').value) * 1000),
+                controls: document.getElementById('courses-add-controls').value.split(',').map((item) => parseInt(item)),
+            })
+            db.saveDatabase()
+            blankCourse()
+        }
+        catch (error) {
+            coursesInfo('error', ' Error: An course with this name already exists')
+        }
+    }
+}
+
+function updateCourse () {
+    coursesInfo('blank')
+    if (document.getElementById('courses-add-name').value === '') {
+        coursesInfo('error', ' Error: No Name specified for Course ')
+    }
+    else {
+        const courseToUpdate = coursesDB.findOne({ $loki: parseInt(document.getElementById('courses-add-id').value) })
+        const originalControls = courseToUpdate.controls
+        try {
+            courseToUpdate.name = document.getElementById('courses-add-name').value
+            courseToUpdate.climb = document.getElementById('courses-add-climb').value
+            courseToUpdate.length = document.getElementById('courses-add-length').value
+            courseToUpdate.controls = document.getElementById('courses-add-controls').value
+            coursesDB.update(courseToUpdate)
+        }
+        catch (error) {
+            coursesInfo('error', ' Error: An course with this name already exists ')
+        }
+        if (original !== courseToUpdate.controls && competitorsDB.find({ name: document.getElementById('courses-add-name').value }).length > 1) {
+            for (competitor of competitorsDB.find({ name: document.getElementById('courses-add-name').value })) {
+                const courseComplete = checkCourse(competitor.download.controls, courseToUpdate.controls)
+                if (courseComplete.errors !== '') {
+                    competitor.download.totalTime = courseComplete[1]
+                }
+                competitorsDB.update(competitor)
+            }
+        }
+        db.saveDatabase()
+        blankCourse()
+        navigatePage('Courses')
+        document.getElementById('courses-add-id').value = ''
+    }
+}
+
+function deleteCourse () {
+    dialogs.openConfirmDialog('Nevis - Delete Course', 'Are you sure you want to Delete this Course?', 'Delete', 'Cancel').then((state) => {
+        if (state) {
+            coursesDB.removeWhere({ $loki: parseInt(document.getElementById('courses-add-id').value) })
+            db.saveDatabase()
+            blankCourse()
+            navigatePage('Courses')
+            coursesInfo('info', ' Course Deleted')
+            document.getElementById('courses-add-id').value = ''
+        }
+    })
+}
+
 // Import course from different file types
 const importXMLCourses = function () {
     dialog.showOpenDialog({
@@ -124,4 +194,22 @@ const deleteAllCourses = function () {
             homeVue.courses = coursesDB.data
         }
     })
+}
+
+// Navigate between add courses and update and refresh the data
+function courseLink (id) {
+    navigatePage('Courses/Update')
+    let linkedCourse = coursesDB.findOne({ $loki: parseInt(id) })
+    document.getElementById('courses-add-id').value = id
+    document.getElementById('courses-add-name').value = linkedCourse.name
+    document.getElementById('courses-add-climb').value = linkedCourse.climb
+    document.getElementById('courses-add-length').value = linkedCourse.length
+    document.getElementById('courses-add-controls').value = linkedCourse.controls
+}
+
+function blankCourse () {
+    document.getElementById('courses-add-name').value = ''
+    document.getElementById('courses-add-climb').value = ''
+    document.getElementById('courses-add-length').value = ''
+    document.getElementById('courses-add-controls').value = ''
 }
