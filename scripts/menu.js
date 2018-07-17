847b028c1b455e05f4eb1fccfe9fc023c916fe23
brownben <9870007+brownben@users.noreply.github.com>
2018-07-17 11:11:08 +0100

    New Interface Design
    - Change Font from Roboto to Montserrat
    - Change the main color to the same color as the logo aand update Logo
    - Update HTML Results
    - Update Full Interface styling including Titlebar and Inputs/ Button
    - Change to CSS Grid for a clearer formatting

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,16 +1,16 @@
 'use strict'
 module.exports.navigatePage = function (page) {
     if (page === 'Welcome') {
-        document.getElementById('welcome').setAttribute('style', 'display:block')
+        document.getElementById('menu-welcome').setAttribute('style', 'display:block')
+        document.getElementById('welcome').setAttribute('style', 'display:grid')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('menu').setAttribute('style', 'display:none')
         document.getElementById('menu-main').setAttribute('style', 'display:none')
         document.getElementById('menu-back').setAttribute('style', 'display:none')
     }
     else if (page === 'Home') {
+        document.getElementById('menu-welcome').setAttribute('style', 'display:none')
         document.getElementById('welcome').setAttribute('style', 'display:none')
-        document.getElementById('home').setAttribute('style', 'display:block')
-        document.getElementById('menu').setAttribute('style', 'display:block')
+        document.getElementById('home').setAttribute('style', 'display:grid')
         document.getElementById('menu-main').setAttribute('style', 'display:block')
         document.getElementById('menu-back').setAttribute('style', 'display:none')
     }
@@ -19,7 +19,7 @@ module.exports.navigatePage = function (page) {
         document.getElementById('menu-download').setAttribute('style', 'display:block')
         document.getElementById('menu-back').setAttribute('style', 'display:block')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('download').setAttribute('style', 'display:block')
+        document.getElementById('download').setAttribute('style', 'display:grid')
     }
     else if (page === 'Entries') {
         entriesAddCourseDropdownRefresh()
@@ -29,9 +29,9 @@ module.exports.navigatePage = function (page) {
         document.getElementById('menu-entries-update').setAttribute('style', 'display:none')
         document.getElementById('menu-back').setAttribute('style', 'display:block')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('entries').setAttribute('style', 'display:block')
+        document.getElementById('entries').setAttribute('style', 'display:grid')
         document.getElementById('entries-info').setAttribute('style', 'display:none')
-        document.getElementById('entries-search').setAttribute('style', 'display:block')
+        document.getElementById('entries-search').setAttribute('style', 'display:grid')
         document.getElementById('entries-add').setAttribute('style', 'display:none')
         document.getElementById('entries-download-data').setAttribute('style', 'display:none')
     }
@@ -43,7 +43,7 @@ module.exports.navigatePage = function (page) {
         document.getElementById('menu-entries-add').setAttribute('style', 'display:block')
         document.getElementById('menu-back').setAttribute('style', 'display:block')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('entries').setAttribute('style', 'display:block')
+        document.getElementById('entries').setAttribute('style', 'display:grid')
         document.getElementById('entries-info').setAttribute('style', 'display:none')
         document.getElementById('entries-search').setAttribute('style', 'display:none')
         document.getElementById('entries-add').setAttribute('style', 'display:block')
@@ -58,7 +58,7 @@ module.exports.navigatePage = function (page) {
         document.getElementById('menu-entries-update').setAttribute('style', 'display:block')
         document.getElementById('menu-back').setAttribute('style', 'display:block')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('entries').setAttribute('style', 'display:block')
+        document.getElementById('entries').setAttribute('style', 'display:grid')
         document.getElementById('entries-info').setAttribute('style', 'display:none')
         document.getElementById('entries-search').setAttribute('style', 'display:none')
         document.getElementById('entries-add').setAttribute('style', 'display:block')
@@ -69,8 +69,8 @@ module.exports.navigatePage = function (page) {
         document.getElementById('menu-back').setAttribute('style', 'display:block')
         document.getElementById('menu-courses').setAttribute('style', 'display:block')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('courses').setAttribute('style', 'display:block')
-        document.getElementById('courses-overview').setAttribute('style', 'display:block')
+        document.getElementById('courses').setAttribute('style', 'display:grid')
+        document.getElementById('courses-overview').setAttribute('style', 'display:grid')
         document.getElementById('menu-courses-add').setAttribute('style', 'display:none')
         document.getElementById('courses-add').setAttribute('style', 'display:none')
         document.getElementById('menu-courses-update').setAttribute('style', 'display:none')
@@ -81,7 +81,7 @@ module.exports.navigatePage = function (page) {
         document.getElementById('menu-back').setAttribute('style', 'display:block')
         document.getElementById('menu-courses').setAttribute('style', 'display:none')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('courses').setAttribute('style', 'display:block')
+        document.getElementById('courses').setAttribute('style', 'display:grid')
         document.getElementById('courses-overview').setAttribute('style', 'display:none')
         document.getElementById('menu-courses-add').setAttribute('style', 'display:block')
         document.getElementById('courses-add').setAttribute('style', 'display:block')
@@ -93,7 +93,7 @@ module.exports.navigatePage = function (page) {
         document.getElementById('menu-back').setAttribute('style', 'display:block')
         document.getElementById('menu-courses').setAttribute('style', 'display:none')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('courses').setAttribute('style', 'display:block')
+        document.getElementById('courses').setAttribute('style', 'display:grid')
         document.getElementById('courses-overview').setAttribute('style', 'display:none')
         document.getElementById('menu-courses-add').setAttribute('style', 'display:none')
         document.getElementById('courses-add').setAttribute('style', 'display:block')
@@ -104,13 +104,13 @@ module.exports.navigatePage = function (page) {
         document.getElementById('menu-back').setAttribute('style', 'display:block')
         document.getElementById('menu-results').setAttribute('style', 'display:block')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('results').setAttribute('style', 'display:block')
+        document.getElementById('results').setAttribute('style', 'display:grid')
     }
     else if (page === 'About') {
         document.getElementById('menu-main').setAttribute('style', 'display:none')
         document.getElementById('menu-back').setAttribute('style', 'display:block')
         document.getElementById('home').setAttribute('style', 'display:none')
-        document.getElementById('about').setAttribute('style', 'display:block')
+        document.getElementById('about').setAttribute('style', 'display:grid')
     }
     else {
         if (currentLocation === 'Entries/Add' || currentLocation === 'Entries/Update') {
@@ -121,9 +121,9 @@ module.exports.navigatePage = function (page) {
             document.getElementById('menu-entries-update').setAttribute('style', 'display:none')
             document.getElementById('menu-back').setAttribute('style', 'display:block')
             document.getElementById('home').setAttribute('style', 'display:none')
-            document.getElementById('entries').setAttribute('style', 'display:block')
+            document.getElementById('entries').setAttribute('style', 'display:grid')
             document.getElementById('entries-info').setAttribute('style', 'display:none')
-            document.getElementById('entries-search').setAttribute('style', 'display:block')
+            document.getElementById('entries-search').setAttribute('style', 'display:grid')
             document.getElementById('entries-add').setAttribute('style', 'display:none')
             document.getElementById('entries-download-data').setAttribute('style', 'display:none')
         }
@@ -132,14 +132,13 @@ module.exports.navigatePage = function (page) {
             document.getElementById('menu-back').setAttribute('style', 'display:block')
             document.getElementById('menu-courses').setAttribute('style', 'display:block')
             document.getElementById('home').setAttribute('style', 'display:none')
-            document.getElementById('courses').setAttribute('style', 'display:block')
-            document.getElementById('courses-overview').setAttribute('style', 'display:block')
+            document.getElementById('courses').setAttribute('style', 'display:grid')
+            document.getElementById('courses-overview').setAttribute('style', 'display:grid')
             document.getElementById('menu-courses-add').setAttribute('style', 'display:none')
             document.getElementById('courses-add').setAttribute('style', 'display:none')
             document.getElementById('menu-courses-update').setAttribute('style', 'display:none')
         }
         else {
-            document.getElementById('menu').setAttribute('style', 'display:block')
             document.getElementById('menu-main').setAttribute('style', 'display:block')
             document.getElementById('menu-download').setAttribute('style', 'display:none')
             document.getElementById('menu-entries').setAttribute('style', 'display:none')
