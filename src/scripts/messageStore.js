d2056616d9537784ca6fc4bfe65a31334942f4a8
brownben <9870007+brownben@users.noreply.github.com>
2018-11-24 19:11:21 +0000

    Testing - Add Time and Message
    - Add tests for time and messages
    - Make sure time is two digits for output

--- src/scripts/messageStore.js
+++ src/scripts/messageStore.js
@@ -1,6 +1,7 @@
 export default {
   messages: [],
   id: 0,
+
   addMessage: function (value, type = 'info') {
     this.messages.push({
       id: this.id,
@@ -9,9 +10,11 @@ export default {
     })
     this.id += 1
   },
+
   clearMessages: function () {
     this.messages.splice(0, this.messages.length)
   },
+
   removeMessage: function (id) {
     this.messages.splice(this.messages.map(message => message.id).indexOf(id), 1)
   },
