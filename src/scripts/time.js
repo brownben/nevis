32c3f80be490e8ed4c97f2233b2c24137237a4f8
brownben <9870007+brownben@users.noreply.github.com>
2018-11-24 19:11:21 +0000

    Testing - Add Time and Message
    - Add tests for time and messages
    - Make sure time is two digits for output

--- src/scripts/time.js
+++ src/scripts/time.js
@@ -1,18 +1,24 @@
+const twoDigits = number => {
+  if (number.toString().length < 2) return '0' + number.toString()
+  else return number
+}
+
 export default {
   elapsed: totalTimeInSeconds => {
     const timeInMinutes = Math.floor(totalTimeInSeconds / 60)
     const timeInSeconds = totalTimeInSeconds % 60
-    return timeInMinutes + ':' + timeInSeconds
+    return twoDigits(timeInMinutes) + ':' + twoDigits(timeInSeconds)
   },
+
   actual: totalTimeInSeconds => {
     const timeInHours = Math.floor(totalTimeInSeconds / 3600)
     const timeInMinutes = Math.floor((totalTimeInSeconds % 3600) / 60)
     const timeInSeconds = (totalTimeInSeconds % 3600) % 60
-    return timeInHours + ':' + timeInMinutes + ':' + timeInSeconds
+    return twoDigits(timeInHours) + ':' + twoDigits(timeInMinutes) + ':' + twoDigits(timeInSeconds)
   },
-  calculateTime: function (download) {
+
+  calculateTime: download => {
     if (download.other !== '') return download.other
-    const time = download.finish - download.start
-    return time
+    return download.finish - download.start
   },
 }
